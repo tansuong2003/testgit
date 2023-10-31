8137d328de95943fd9c44f32d276bc2d6dca19c8
tansuong2003 <tansuong2003@gmail.com>
2023-10-31 14:53:44 +0700

    dalamxong1 phan2

--- De1_NguyenTanSuong_21133078/Form1.cs
+++ De1_NguyenTanSuong_21133078/Form1.cs
@@ -61,7 +61,52 @@ namespace De1_NguyenTanSuong_21133078
 
         private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            var query = from v in context.Vi
+                        join hv in context.Hang on v.MaHang equals hv.MaHang
+                        join lv in context.LoaiVi on v.MaLoai equals lv.MaLoai
+                        select new { TenVi = v.TenVi, TenHang = hv.TenHang, MauSac = v.MauSac, Gia = v.Gia, TenLoai = lv.TenLoai };
 
+            dataGridView1.DataSource = query.ToList();
         }
+        var query = from v in context.Vi
+                    join hv in context.Hang on v.MaHang equals hv.MaHang
+                    join lv in context.LoaiVi on v.MaLoai equals lv.MaLoai
+                    select new { TenVi = v.TenVi, TenHang = hv.TenHang, MauSac = v.MauSac, Gia = v.Gia, TenLoai = lv.TenLoai };
+
+        dataGridView1.DataSource = query.ToList();
+        }
+    var query = from v in context.Vi
+                join hv in context.Hang on v.MaHang equals hv.MaHang
+                join lv in context.LoaiVi on v.MaLoai equals lv.MaLoai
+                select new { TenVi = v.TenVi, TenHang = hv.TenHang, MauSac = v.MauSac, Gia = v.Gia, TenLoai = lv.TenLoai };
+
+    dataGridView1.DataSource = query.ToList();
+        }
+var query = from v in context.Vi
+            join hv in context.Hang on v.MaHang equals hv.MaHang
+            join lv in context.LoaiVi on v.MaLoai equals lv.MaLoai
+            select new { TenVi = v.TenVi, TenHang = hv.TenHang, MauSac = v.MauSac, Gia = v.Gia, TenLoai = lv.TenLoai };
+
+dataGridView1.DataSource = query.ToList();
+        } var query = from v in context.Vi
+                      join hv in context.Hang on v.MaHang equals hv.MaHang
+                      join lv in context.LoaiVi on v.MaLoai equals lv.MaLoai
+                      select new { TenVi = v.TenVi, TenHang = hv.TenHang, MauSac = v.MauSac, Gia = v.Gia, TenLoai = lv.TenLoai };
+
+dataGridView1.DataSource = query.ToList();
+        } var query = from v in context.Vi
+                      join hv in context.Hang on v.MaHang equals hv.MaHang
+                      join lv in context.LoaiVi on v.MaLoai equals lv.MaLoai
+                      select new { TenVi = v.TenVi, TenHang = hv.TenHang, MauSac = v.MauSac, Gia = v.Gia, TenLoai = lv.TenLoai };
+
+dataGridView1.DataSource = query.ToList();
+        } var query = from v in context.Vi
+                      join hv in context.Hang on v.MaHang equals hv.MaHang
+                      join lv in context.LoaiVi on v.MaLoai equals lv.MaLoai
+                      select new { TenVi = v.TenVi, TenHang = hv.TenHang, MauSac = v.MauSac, Gia = v.Gia, TenLoai = lv.TenLoai };
+
+dataGridView1.DataSource = query.ToList();
+        }
+}
     }
 }
